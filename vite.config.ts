6023ee86b873a88307513e8d67a1279d44787e7d
markyong <markyong14@gmail.com>
2026-07-04 15:03:20 +0800

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   base: "/kindle-dashboard/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["ie >= 11"],
+      renderLegacyChunks: true,
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
 });
